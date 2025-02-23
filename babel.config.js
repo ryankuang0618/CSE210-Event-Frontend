362546e602bc6a7c91d3624073be66d631b67c75
Gokul G <gokulg232003@gmail.com>
2025-02-22 17:20:06 -0800

    Update jest.setup.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: [
       '@babel/preset-env', // For ES6+ support
+      '@babel/preset-typescript', // For TypeScript support
       [
         '@babel/preset-react', // For React JSX support
         { "runtime": "automatic" } // For React 17 JSX support
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
-
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',  // Set the test environment
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // Use babel-jest for transformation
   },
+  moduleNameMapper: {
+    // Handle CSS Modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Mock regular CSS imports
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Mock static assets (e.g., images)
+    '^.+\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],  // Recognize .tsx files
+  setupFiles: ['<rootDir>/jest.setup.js'],  // Add setupFiles for global polyfills
   setupFilesAfterEnv: ['@testing-library/jest-dom'],  // Setup testing library
 };
